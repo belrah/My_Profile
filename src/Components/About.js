167bd845c016ec4cc90551e13172029c34ff809c
belrah <belrahj3@gmail.com>
2022-03-13 19:59:09 +0100

    modified the home page

--- src/Components/About.js
+++ src/Components/About.js
@@ -30,7 +30,6 @@ class About extends Component {
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
 						   <span>{street}<br />
 						         {city} {state}, {zip}
                    </span><br />
